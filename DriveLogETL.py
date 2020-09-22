2dc4d46deda833305f03bc5ca22c6a6be4c21dc9
Sanket Chobe <sanketchobe@Sankets-MacBook-Pro.local>
2020-09-21 22:01:33 -0500

    First version of ETL pipeline changes

---
+++ DriveLogETL.py
@@ -0,0 +1,290 @@
+#import configparser
+#from datetime import datetime
+import json
+import os
+
+import snowflake.connector
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+
+def set_up_env():
+    '''
+    function: to set credentials for AWS
+    '''
+
+    config = json.load(open('AWS.json'))
+
+    os.environ['AWS_ACCESS_KEY_ID'] = config['AWS']['AWS_ACCESS_KEY_ID']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    '''
+    function: instantiate spark session
+    '''
+
+    spark = SparkSession\
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.5") \
+        .config("spark.jars","/Users/sanketchobe/Downloads/spark-3.0.1-bin-hadoop2.7/jars/snowflake-jdbc-3.12.8.jar,/Users/sanketchobe/Downloads/spark-3.0.1-bin-hadoop2.7/jars/spark-snowflake_2.12-2.8.1-spark_3.0.jar") \
+        .getOrCreate()
+
+    return spark
+
+
+def data_Extract(spark, data_source, data,error_log_path):
+
+    if data_source =='csv':
+        if 'date' in data:
+            date = str(data['date'])
+        else:
+            date = ''
+
+        if 'time' in data:
+            hour = str(data['time'])
+        else:
+            hour = '00'
+
+        path =data['path']
+        csv_schema =StructType([
+                    StructField("v_id", StringType()),
+                    StructField("fn_id", StringType()),
+                    StructField("status", StringType()),
+                    StructField("time", FloatType())
+                    ])
+        try:
+            df = spark.read.csv(path, header=True, nullValue='NA', schema=csv_schema)
+            df.show(5)
+            df.printSchema()
+        except Exception as excpt:
+            error_log_path.write(str(excpt))
+
+        return (date, hour, df)
+    else:
+        error_log_path.write("Unsupported source data type, please check the correct format..")
+
+    '''
+        filler code for additional data sources like parquet, json and DB tables
+    if data_source =='parquet':
+    if data_source =='json'
+    '''
+
+
+
+def data_Transform(spark, extract_data):
+    date, hour,df = extract_data[0], extract_data[1], extract_data[2]
+    print(date, hour)
+    df.createOrReplaceTempView("extract_data")
+
+    lr_df1 = spark.sql("""select v_id,
+                                fn_id,
+                                status,
+                                case 
+                                    when '{0}'  ='' then current_date() 
+                                    else to_date('{0}')
+                                end as date,   
+                                cast({1} as string) as hour, 
+                                time,
+                                from_unixtime(cast(time*60 as string), 'mm:ss.SSSSSS') as minutes
+                           from extract_data""".format(date, hour))
+
+    lr_df1.createOrReplaceTempView("curr_tb")
+
+    lr_df2 = spark.sql("""select v_id,
+                                fn_id,
+                                status,
+                                to_timestamp(concat(cast(date as string), ' ', cast(hour as string),':', 
+                                 cast(minutes as string))) as execution_timestmp
+                           from curr_tb
+                           where date = current_date()""".format(date, hour))
+
+    lr_df3 = spark.sql("""select v_id,
+                                 fn_id,
+                                 status,
+                                 date,   
+                                 hour, 
+                                 round(time *60) as time, 
+                                 to_timestamp(concat(cast(date as string), ' ', cast(hour as string),':', 
+                                 cast(minutes as string))) as execution_timestmp,
+                                 rank() OVER (Partition by v_id, fn_id, status order by time desc) as log_rank 
+                            from curr_tb 
+                        """.format(date, hour))
+
+    lr_df3.createOrReplaceTempView("log_hotspots")
+
+    lr_df4 = spark.sql("""select a.v_id,
+                                 a.fn_id,
+                                 abs(a.time - b.time) as execution_time,
+                                 a.execution_timestmp
+                            from log_hotspots a,
+                                 log_hotspots b
+                            where a.v_id =b.v_id
+                              and a.fn_id = b.fn_id
+                              and a.log_rank = b.log_rank
+                              and a.status ='start'
+                              and b.status ='end'
+                       """)
+
+    return (lr_df1, lr_df2, lr_df4)
+
+def create_database(snow_connection, error_log_path):
+    print(snow_connection)
+
+    snow_connection.cursor().execute("use role ACCOUNTADMIN")
+    snow_connection.cursor().execute("CREATE WAREHOUSE IF NOT EXISTS Drive_Analysis_WH")
+    snow_connection.cursor().execute("CREATE DATABASE IF NOT EXISTS Drive_DB")
+    snow_connection.cursor().execute("USE DATABASE Drive_DB")
+    snow_connection.cursor().execute("CREATE SCHEMA IF NOT EXISTS Drive_USE_Log")
+
+    snow_connection.cursor().execute("USE DATABASE Drive_DB")
+    snow_connection.cursor().execute("USE WAREHOUSE Drive_Analysis_WH")
+    snow_connection.cursor().execute("USE SCHEMA Drive_DB.Drive_USE_Log")
+
+    try:
+        snow_connection.cursor().execute("CREATE TABLE IF NOT EXISTS "
+                                         "Vehicle( vehicle_id varchar(50), "
+                                         "vehicle_name varchar(50),"
+                                         "vehicle_type varchar(50))"
+                                         )
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+        print(str(excpt))
+
+    try:
+        snow_connection.cursor().execute("CREATE TABLE IF NOT EXISTS "
+                                         "Function(function_id varchar(50),"
+                                         "function_name varchar(50),"
+                                         "function_type varchar(50))"
+                                         )
+    except Exception as excpt:
+        error_log_path.write(excpt)
+        print("error in Function table creation")
+
+    try:
+        snow_connection.cursor().execute("CREATE TABLE IF NOT EXISTS "
+                                         "Status(status varchar(10),"
+                                         "status_desc varchar(50))"
+                                         )
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+        print(str(excpt))
+
+    try:
+        snow_connection.cursor().execute("CREATE TABLE IF NOT EXISTS "
+                                         "Drive_Log( vehicle_Id varchar(30),"
+                                         "function_Id varchar(30), "
+                                         "status varchar(10),"
+                                         "log_date DATE,"
+                                         "log_hour varchar(2),"
+                                         "log_time varchar(30),"
+                                         "log_minutes varchar(30))"
+                                         )
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+        print(str(excpt))
+
+    try:
+        snow_connection.cursor().execute("CREATE TABLE IF NOT EXISTS "
+                                         "Drive_Log_Daily(vehicle_Id varchar(30),"
+                                         "function_Id varchar(30),"
+                                         "status varchar(10),"
+                                         "log_timestamp timestamp)"
+                                         )
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+        print(str(excpt))
+
+    try:
+        snow_connection.cursor().execute("CREATE TABLE IF NOT EXISTS "
+                                         "Drive_Hotspots(vehicle_Id varchar(30),"
+                                         "function_Id varchar(30),"
+                                         "execution_time varchar(10),"
+                                         "log_timestamp timestamp)"
+                                         )
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+        print(str(excpt))
+
+
+def load_tables(snow_connect,spark, full_tb, curr_tb, hotspot_tb, error_log_path):
+    spark_conf = SparkConf().setMaster('local').setAppName('Drive_Log')
+    snowflake_source = "net.snowflake.spark.snowflake"
+
+    sfOptions = {
+        "sfURL": "zi71392.us-east-2.aws.snowflakecomputing.com",
+        "sfUser": snow_connect["userid"],
+        "sfPassword": snow_connect["password"],
+        "sfDatabase": "Drive_DB",
+        "sfSchema": "Drive_USE_Log",
+        "sfWarehouse": "Drive_Analysis_WH",
+        "sfRole":"ACCOUNTADMIN"
+    }
+
+    print(sfOptions)
+
+    full_tb.show(5)
+    curr_tb.show(5)
+    hotspot_tb.show(5)
+    try:
+        full_tb.write.format(snowflake_source)\
+            .options(**sfOptions)\
+            .option("dbtable", "Drive_Log")\
+            .mode("append").save()
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+
+    try:
+        curr_tb.write.format(snowflake_source)\
+            .options(**sfOptions)\
+            .option("dbtable", "Drive_Log_Daily")\
+            .mode("append").save()
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+
+    try:
+        hotspot_tb.write.format(snowflake_source)\
+            .options(**sfOptions)\
+            .option("dbtable", "Drive_Hotspots")\
+            .mode("append").save()
+    except Exception as excpt:
+        error_log_path.write(str(excpt))
+
+
+def data_Load(spark, full_tb, curr_tb, hotspot_tb,error_log_path):
+    snow_connect = json.load(open('Configs/snow_connect.json'))
+    print(snow_connect)
+    snow_connection = snowflake.connector.connect(
+        user=snow_connect["userid"],
+        password=snow_connect["password"],
+        account=snow_connect["account"],
+        session_parameters={
+            "QUERY_TAG": "DriveLogWH",
+        }
+    )
+
+    create_database(snow_connection, error_log_path)
+    load_tables(snow_connect,spark, full_tb, curr_tb, hotspot_tb, error_log_path )
+
+
+def main():
+    #set_up_env()
+    spark_session = create_spark_session()
+
+    etl_data = json.load(open('Configs/data_source.json'))
+    error_log = json.load(open('Configs/error_log.json'))
+    error_log_path = open(error_log["error_log"],"a")
+
+    for dataSource, dataSet in etl_data['data_sources'].items():
+        print('data source:', dataSource)
+        print('data:',dataSet)
+        extract_data = data_Extract(spark_session, dataSource, dataSet,error_log_path)
+        full_tb, curr_tb, hotspot_tb= data_Transform(spark_session, extract_data)
+        print(full_tb, curr_tb, hotspot_tb)
+        data_Load(spark_session, full_tb, curr_tb, hotspot_tb,error_log_path)
+        error_log_path.close()
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# DriveLogETL
+# Drive Log ETL
+
+An ETL pipeline for loading the Drive logs for analytics.. 
+
+Important components.
+- Star/ Snowflake Schema for Data model 
+- Spark with Python
+- AWS (for scalable pipeline)
+- Snowflake (Datawarehouse)
+
+**Assumptions**
+
+*Assumptions on Source Data and Format*
+- I am assuming the input data is available in multiple source formats including CSV, Parquet and S3 files and databases like MySql etc. I am using only CSV file for this ETL pipeline.
+- Since the logs are offloaded daily, we can assume that the input log data file can have 3 different parts. Date, Hour and the input file name. For this ETL pipeline, I will keep 3 different parts for a source file, Date, Hour and file name. 
+- Each file name represents the log offloaded at a particular date at particular time. For example, Date: 2020-01-01, Hour:17, Filename: 2020-01-01_17.csv represnts the log offloaded on 2020-01-01 at 17 hours. 
+- This helps in maintaining the daily log data at particular hours in particular format. We can perform ETL on the data to load data in datawarehouse for daily logs, all logs and derrive particular insights like hotspots on daily as well as all time data.
+
+*Assumptions on Target Data and Format*
+- We are assuming the data is needed by Data Scientists and Analysts in the format of reports like daily, monthly or yearly Drive Logs and Drive Hotspots. 
+- These reports can be used to perform analysis and visualize a particular function that takes large amount of time to complete in the particular time period, and understand the pattern of execution of different functions for different type of vehicles. 
+- Considering the above requirements for analysis, I am making a star schema for the data model. The data model should give correct snapshot of the historical logs, daily logs and aggregated facts like hotspots. 
+- I am assuming the source data is reported as a daily or hourly file stored in multiple formats, it is important to design an ETL framework which will extract data from multiple sources and formats and load in a datawarehouse which supports this variety of information. 
+ 
+*Design Assumptions*
+1. Scalability 
+    - I am assuming that the ETL pipeline should be highly scalable. Since the logs are offloaded on daily basis and the amount of logs can be in TBs, we should make sure the ETL pipeline can handle the large amount of data on daily basis and should be able to perform ETL and load operations for huge amount of data. 
+    - I am going to use Spark framework to process the large amount of data on regular basis. For the initial implementation, I am implementing the pipeline using the Spark framework to run the ETL pipeline and load in Snowflake datawarehouse. 
+    - Spark supports the scalability for the TBs of data and Snowflake datawarehouse also supports the scalability for large amount of data in multiple formats. 
+2. Consistency
+    - Snowflake datawarehouse supports consistency for the data as it supports multiple formats including structured and semi-structured type of data.
+    - The data models in the Snowflake datawarehouse are based on the relational DB models hence it supports concurrency as well as consistency for large amount of data.
+    - for ETL framework Spark in association with AWS services like AWS EMR or AWS EC2 can support consistency for huge amount of data being processed in Spark by adding the nodes and cluster to process this data. 
+3. Persistence
+    - Snowflake datawarehouse supports the persistence for large amount of data. Since the Snowflake has compute and storage as separate services, and Snowflake supports multiple clusters for each of these functions, the data persistence for such large data is supported by Snowflake. 
+    - Spark with AWS services like EMR and EC2 supports persistence for large amount of data. Since AWS provides multi-region support, it supports the persistence of ETL functionality.  
+4. Availability 
+    - Snowflake datawarehouse supports the high availability of data since the data storage and computations are separate, and Snowflake supports multi-cluster and multi-region functionality. 
+    - Spark with AWS provides high availability for the ETL pipeline since it can handle TBs of data with data backup using multi-region functionality. 
+     
+5. Optimization and Cost
+    - Though AWS with Spark supports different opportunities for optimization, it may be costly to debug and resolve the errors in the Spark or AWS environments.
+    - For this ETL pipeline considering the requirements of analytics on large amount of data, I am not thinking of cost effective solution. It makes sense to consider the ETL pipeline supporting variety of data source formats, process this data and load in a consistent, scalable datawarehouse solution. 
+    
+6. Historical Data
+    - Snowflake supports the storage of data as historical information. It supports the storage as a cloud service and in different formats. 
+    - The data storage for historical data can be retrived later for the analyzing the historical data and building out the functions, models for data analysis. 
+
+I am explaining my ETL pipeline using the following components and architecture. 
+Pre-requisites:
+
+1. Install and configure Spark. 
+2. Install and configure Snowflake - free/enterprise set up.
+3. Install and configure Spark-Snowflake connectivity. 
+
+Additional Pre-requisites:
+1. Set up and configure AWS EMR and AWS s3 to set up ETL pipeline on AWS. 
+
+**Star Schema for Datawarehouse**
+![Screenshot](/Users/sanketchobe/Desktop/ETL_star_schema.png)
+
+*Data Model*
+- Vehicle: This table is a dimensional table containing the attributes for Vehicle like Vehicle name or Vehicle type.
+- Function: This table is a dimensional table containing the attributes for Function, like function name, function type, etc. 
+- Status: This table is a dimensional table containing the attributes for Status, like status (Start, End), status type, etc. 
+- Time: This table is a dimensional table containing the time related attributes like year, date, month, day, hour, minutes, and timestamp.
+- Driver_Log: This table is a fact table containing the keys from each of the dimensional table like vehicle_id, function_id etc. This table represents the core data for drive logs for all time.
+- Drive_log_Daily: This table is a derived table or view which represents the drive log on daily basis. 
+- Drive_Hotspots: This table represents aggregated fact or view showing the drive hotspots for any function. We can derive the daily hotspots,monthly hotspots, etc. which can be further used by Data Scientists to perform analysis or visualize data. 
+
+*ETL Pipeline*
+- Extract Data: This step extracts the data from multiple sources like CSV, parquet or json. This step will also extract the information for date and hour for which we are going to load the log.
+- Transform Data: This step will convert the extracted data and convert in the required format, like converting the time in seconds to minutes and creating the timestamp from date, hour and minutes. I used the PySpark and SparkSQL code to create the load ready dataframes for each of the table Drive_log, Drive_Log_Daily, and Drive_Hotspots explained in the data model. 
+- Load Data: This step will load the LR dataframes into the Snowflake tables. I used the Spark Snowflake connector to connect with the Snowflake datawarehouse and load in individual table directly from the dataframe. 
+  
+
+**Software Architecture**
+![Screenshot](/Users/sanketchobe/Desktop/ETL_architecture.png)
+
+
+Run ETL pipeline
+
+
+
